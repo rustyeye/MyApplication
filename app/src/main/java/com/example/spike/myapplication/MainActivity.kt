8bcba0807fb66da8600b3f4a7f0e0678d054d050
rustyeye <rusty.eyes@gmail.com>
2018-06-04 13:46:35 +0900

    test

--- app/src/main/java/com/example/spike/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/spike/myapplication/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.spike.myapplication
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 
 import com.example.spike.myapplication.databinding.ActivityMainBinding
 import com.example.spike.myapplication.model.User
+import com.example.spike.myapplication.view.SubActivity1
+import com.example.spike.myapplication.view.SubActivity2
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var user : User = User("신성환", 0)
+    private var user : User = User("User0", 0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,62 +27,58 @@ class MainActivity : AppCompatActivity() {
         //binding.setUser(user)
         binding.setVariable(BR.user, user)
         binding.executePendingBindings()
+    }
 
-//        textview.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//            //textview.setText("Hello World!")
-//            user = User("Hello World!", 0)
-//        }
-//
-//        button1.setOnClickListener {
-//            //textview.setText("Button1")
-//            setUser("Button1", 1)
-//        }
-//
-//        button2.setOnClickListener {
-//            //textview.setText("Button2")
-//            setUser("Button2", 2)
-//        }
-//
-//        button3.setOnClickListener {
-//            //textview.setText("Button3")
-//            setUser("Button3", 3)
-//        }
-//
-//        button4.setOnClickListener {
-//            //textview.setText("Button4")
-//            setUser("Button4", 4)
-//        }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (resultCode) {
+            0 -> Toast.makeText(this, "$requestCode", Toast.LENGTH_LONG).show()
+            1 -> gotoSub1()
+            2 -> gotoSub2()
+        }
     }
 
     fun onTextViewClicked(view: View) {
         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
 
-        user = User("Hello World!", 0)
-        //binding.setUser(user)
+        if (user.name.get()!!.startsWith("Hello"))
+            return
+
+        user = User("Hello ${user.name.get()}!", 0)
         binding.setVariable(BR.user, user)
+        //binding.setUser(user)
     }
 
     fun onClicked1(view: View) {
-        setUser("Button1", 1)
+        gotoSub1()
     }
 
     fun onClicked2(view: View) {
-        setUser("Button2", 1)
-    }
-
-    fun onClicked3(view: View) {
-        setUser("Button3", 1)
-    }
-
-    fun onClicked4(view: View) {
-        setUser("Button4", 1)
+        gotoSub2()
     }
 
     fun setUser(name: String, age: Int) {
+        //user = User(name, age)
+        //binding.setVariable(BR.user, user)
+        //binding.setUser(user)
+
         user.name.set(name)
         user.age.set(age)
+    }
+
+    fun gotoSub1() {
+        val intent = Intent(this, SubActivity1::class.java)
+        intent.putExtra("user.name", user.name.get())
+        intent.putExtra("user.age", user.age.get())
+        startActivityForResult(intent, 1)
+    }
+
+    fun gotoSub2() {
+        val intent = Intent(this, SubActivity2::class.java)
+        intent.putExtra("user.name", user.name.get())
+        intent.putExtra("user.age", user.age.get())
+        startActivityForResult(intent, 2)
     }
 }
--- app/src/main/java/com/example/spike/myapplication/model/User.kt
+++ app/src/main/java/com/example/spike/myapplication/model/User.kt
@@ -1,10 +1,11 @@
 package com.example.spike.myapplication.model
 
 import android.databinding.ObservableField
+import android.databinding.ObservableInt
 
 class User (name : String, age : Int) {
     var name = ObservableField<String>()
-    var age = ObservableField<Int>()
+    var age = ObservableInt()
 
     init {
         this.name.set(name)
